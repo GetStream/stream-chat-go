72cf28e5e06ce2551dbf13437b1776babb923cb0
Bogdan D <dmbohdan@gmail.com>
2019-08-05 23:13:13 +0300

    add device_test

---
+++ device_test.go
@@ -0,0 +1,40 @@
+package stream_chat
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_Devices(t *testing.T) {
+	c, _ := initClient(t)
+
+	user := randomUser()
+
+	resp, err := c.GetDevices(user.ID)
+	mustNoError(t, err)
+
+	if !assert.Len(t, resp, 0, "devices are not empty in response") {
+		t.FailNow()
+	}
+
+	devices := []Device{
+		{UserID: user.ID, ID: randomString(12), PushProvider: PushProviderFirebase},
+		{UserID: user.ID, ID: randomString(12), PushProvider: PushProviderAPNS},
+	}
+
+	for _, dev := range devices {
+		err = c.AddDevice(dev)
+		mustNoError(t, err)
+
+		resp, err = c.GetDevices(user.ID)
+		mustNoError(t, err)
+
+		if !assert.Len(t, resp, 1) {
+			t.FailNow()
+		}
+
+		err = c.DeleteDevice(user.ID, dev.ID)
+		mustNoError(t, err)
+	}
+}
