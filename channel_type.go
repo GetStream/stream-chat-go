416a032d9c8b1862f8317aa704cf40592657bfc2
Bogdan D <dmbohdan@gmail.com>
2019-08-06 23:34:11 +0300

    update channel type

--- channel_type.go
+++ channel_type.go
@@ -1,30 +1,58 @@
 package stream_chat
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 type ChannelType struct {
+	Name             string
+	TypingEvents     bool
+	ReadEvents       bool
+	ConnectEvents    bool
+	Search           bool
+	Reactions        bool
+	Replies          bool
+	Mutes            bool
+	MessageRetention string
+	MaxMessageLength int
+	Automod          string
+	AutomodBehaviour string
+	Commands         []interface{}
+	Permissions      []interface{}
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
 }
 
 // CreateChannelType adds new channel type
-func (c *Client) CreateChannelType(data map[string]interface{}) (err error) {
-	if data["commands"] == "" {
-		data["commands"] = "all"
+func (c *Client) CreateChannelType(chType ChannelType) (err error) {
+	if len(chType.Commands) == 0 {
+		chType.Commands = []interface{}{"all"}
 	}
+
 	err = c.makeRequest(http.MethodPost, "channeltypes", nil, data, nil)
 
 	return
 }
 
 // GetChannelType returns information about channel type
-func (c *Client) GetChannelType(chanType string) (resp map[string]interface{}, err error) {
-	err = c.makeRequest(http.MethodGet, "channeltypes/"+chanType, nil, nil, &resp)
+func (c *Client) GetChannelType(chanType string) (ChannelType, error) {
+	var resp struct {
+		ChannelTypes map[string]ChannelType `json:"channel_types"`
+	}
 
-	return
+	err := c.makeRequest(http.MethodGet, "channeltypes/"+chanType, nil, nil, &resp)
+
+	return resp.ChannelTypes[chanType], err
 }
 
 // ListChannelTypes returns all channel types
-func (c *Client) ListChannelTypes() (resp map[string]interface{}, err error) {
-	err = c.makeRequest(http.MethodGet, "channeltypes", nil, nil, &resp)
+func (c *Client) ListChannelTypes() (map[string]ChannelType, error) {
+	var resp struct {
+		ChannelTypes map[string]ChannelType `json:"channel_types"`
+	}
 
-	return
+	err := c.makeRequest(http.MethodGet, "channeltypes", nil, nil, &resp)
+
+	return resp.ChannelTypes, err
 }
---
+++ channel_type_test.go
@@ -0,0 +1,17 @@
+package stream_chat
+
+import (
+	"testing"
+)
+
+func TestClient_CreateChannelType(t *testing.T) {
+
+}
+
+func TestClient_GetChannelType(t *testing.T) {
+
+}
+
+func TestClient_ListChannelTypes(t *testing.T) {
+
+}
