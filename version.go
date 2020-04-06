e0392a7b3bc1c74787b61367ab53432b7cd8dac2
ferhat elmas <elmas.ferhat@gmail.com>
2020-04-06 10:52:24 +0200

    Add version client header
    fixes #63

---
+++ version.go
@@ -0,0 +1,27 @@
+package stream_chat //nolint: golint
+
+import (
+	"fmt"
+)
+
+const (
+	versionMajor = 2
+	versionMinor = 1
+	versionPatch = 0
+)
+
+// Version returns the version of the library
+func Version() string {
+	return "v" + fmtVersion()
+}
+
+func versionHeader() string {
+	return "stream-go-client-" + fmtVersion()
+}
+
+func fmtVersion() string {
+	return fmt.Sprintf("%d.%d.%d",
+		versionMajor,
+		versionMinor,
+		versionPatch)
+}
