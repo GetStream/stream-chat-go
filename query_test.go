506211196fc0c0d02547c4ddb06db6f16e82052a
ferhat elmas <elmas.ferhat@gmail.com>
2020-07-17 22:46:17 +0200

    Handle offset and limit for query users

--- query_test.go
+++ query_test.go
@@ -2,6 +2,7 @@ package stream_chat // nolint: golint
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -10,19 +11,57 @@ import (
 func TestClient_QueryUsers(t *testing.T) {
 	c := initClient(t)
 
-	user := randomUser()
+	const n = 4
+	ids := make([]string, n)
+	defer func() {
+		for _, id := range ids {
+			if id != "" {
+				_ = c.DeleteUser(id, nil)
+			}
+		}
+	}()
 
-	users, err := c.QueryUsers(&QueryOption{Filter: map[string]interface{}{
-		"id": map[string]interface{}{
-			"$eq": user.ID,
-		}},
+	for i := n - 1; i > -1; i-- {
+		u := &User{ID: randomString(30), ExtraData: map[string]interface{}{"order": n - i - 1}}
+		_, err := c.UpdateUser(u)
+		require.NoError(t, err)
+		ids[i] = u.ID
+		time.Sleep(200 * time.Millisecond)
+	}
+
+	t.Parallel()
+	t.Run("Query all", func(tt *testing.T) {
+		results, err := c.QueryUsers(&QueryOption{Filter: map[string]interface{}{
+			"id": map[string]interface{}{
+				"$in": ids,
+			}},
+		})
+
+		require.NoError(tt, err)
+		require.Len(tt, results, len(ids))
 	})
 
-	require.NoError(t, err, "query users error")
+	t.Run("Query with offset/limit", func(tt *testing.T) {
+		offset := 1
 
-	if assert.NotEmpty(t, users, "query users exists") {
-		assert.Equal(t, user.ID, users[0].ID, "received user ID")
-	}
+		results, err := c.QueryUsers(
+			&QueryOption{
+				Filter: map[string]interface{}{
+					"id": map[string]interface{}{
+						"$in": ids,
+					},
+				},
+				Offset: offset,
+				Limit:  2,
+			},
+		)
+
+		require.NoError(tt, err)
+		require.Len(tt, results, 2)
+
+		require.Equal(tt, results[0].ID, ids[offset])
+		require.Equal(tt, results[1].ID, ids[offset+1])
+	})
 }
 
 func TestClient_QueryChannels(t *testing.T) {
