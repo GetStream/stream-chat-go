d64b946090fbc7fb01dbced373edc7339ecd2dac
Guyon Morée <guyon.moree@gmail.com>
2021-10-18 14:03:00 +0200

    [CHAT-2191] Async DeleteUsers (#140)
    Co-authored-by: ferhat elmas <elmas.ferhat@gmail.com>

--- async_tasks_test.go
+++ async_tasks_test.go
@@ -2,6 +2,7 @@ package stream_chat //nolint: golint
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -14,7 +15,7 @@ func TestClient_DeleteChannels(t *testing.T) {
 
 	msg := &Message{Text: "test message"}
 
-	msg, err := ch.SendMessage(msg, user.ID, MessageSkipPush)
+	_, err := ch.SendMessage(msg, user.ID, MessageSkipPush)
 	require.NoError(t, err, "send message")
 
 	// should fail without CIDs in parameter
@@ -25,8 +26,57 @@ func TestClient_DeleteChannels(t *testing.T) {
 	require.NoError(t, err)
 	require.NotEmpty(t, taskID)
 
-	resp, err := c.GetTask(taskID)
+	for i := 0; i < 10; i++ {
+		resp, err := c.GetTask(taskID)
+		require.NoError(t, err)
+		require.Equal(t, taskID, resp.TaskID)
+
+		if resp.Status == "completed" {
+			require.Equal(t, resp.Result[ch.CID], map[string]interface{}{"status": "ok"})
+			return
+		}
+
+		time.Sleep(time.Second)
+	}
+}
+
+func TestClient_DeleteUsers(t *testing.T) {
+	c := initClient(t)
+	ch := initChannel(t, c)
+
+	user := randomUser()
+
+	msg := &Message{Text: "test message"}
+
+	_, err := ch.SendMessage(msg, user.ID, MessageSkipPush)
+	require.NoError(t, err, "send message")
+
+	// should fail without userIDs in parameter
+	_, err = c.DeleteUsers([]string{}, DeleteUserOptions{
+		User:     SoftDelete,
+		Messages: HardDelete,
+	})
+	require.Error(t, err)
+
+	taskID, err := c.DeleteUsers([]string{user.ID}, DeleteUserOptions{
+		User:     SoftDelete,
+		Messages: HardDelete,
+	})
 	require.NoError(t, err)
-	require.Equal(t, taskID, resp.TaskID)
-	require.NotEmpty(t, resp.Status)
+	require.NotEmpty(t, taskID)
+
+	for i := 0; i < 10; i++ {
+		resp, err := c.GetTask(taskID)
+		require.NoError(t, err)
+		require.Equal(t, taskID, resp.TaskID)
+
+		if resp.Status == "completed" {
+			require.Equal(t, resp.Result[user.ID], map[string]interface{}{"status": "ok"})
+			return
+		}
+
+		time.Sleep(time.Second)
+	}
+
+	require.True(t, false, "task did not succeed")
 }
