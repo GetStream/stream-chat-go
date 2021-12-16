dc10bbf94d316234e29890fa464d17aba5eabd2d
yaziine <y.ennebati@gmail.com>
2021-12-16 11:35:22 +0400

    wip

--- async_tasks.go
+++ async_tasks.go
@@ -20,38 +20,40 @@ const (
 	TaskStatusFailed    TaskStatus = "failed"
 )
 
-type Task struct {
+type TaskResponse struct {
 	TaskID    string     `json:"task_id"`
 	Status    TaskStatus `json:"status"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
 
 	Result map[string]interface{} `json:"result,omitempty"`
+	Response
 }
 
 // GetTask returns the status of a task that has been ran asynchronously.
-func (c *Client) GetTask(ctx context.Context, id string) (*Task, error) {
+func (c *Client) GetTask(ctx context.Context, id string) (*TaskResponse, error) {
 	if id == "" {
 		return nil, fmt.Errorf("id should not be empty")
 	}
 
 	p := path.Join("tasks", url.PathEscape(id))
 
-	var task Task
+	var task TaskResponse
 	err := c.makeRequest(ctx, http.MethodGet, p, nil, nil, &task)
 	return &task, err
 }
 
 type AsyncTaskResponse struct {
 	TaskID string `json:"task_id"`
+	Response
 }
 
 // DeleteChannels deletes channels asynchronously.
 // Channels and messages will be hard deleted if hardDelete is true.
-// It returns a task ID, the status of the task can be check with client.GetTask method.
-func (c *Client) DeleteChannels(ctx context.Context, cids []string, hardDelete bool) (string, error) {
+// It returns an AsyncTaskResponse object which contains the task ID, the status of the task can be check with client.GetTask method.
+func (c *Client) DeleteChannels(ctx context.Context, cids []string, hardDelete bool) (*AsyncTaskResponse, error) {
 	if len(cids) == 0 {
-		return "", fmt.Errorf("cids parameter should not be empty")
+		return nil, fmt.Errorf("cids parameter should not be empty")
 	}
 
 	data := struct {
@@ -64,7 +66,7 @@ func (c *Client) DeleteChannels(ctx context.Context, cids []string, hardDelete b
 
 	var resp AsyncTaskResponse
 	err := c.makeRequest(ctx, http.MethodPost, "channels/delete", nil, data, &resp)
-	return resp.TaskID, err
+	return &resp, err
 }
 
 type DeleteType string
@@ -86,10 +88,10 @@ type DeleteUserOptions struct {
 // Conversations (1to1 channels) will be deleted if either "hard" or "soft"
 // Messages will be deleted if either "hard" or "soft"
 // NewChannelOwnerID any channels owned by the hard-deleted user will be transferred to this user ID
-// It returns a task ID, the status of the task can be check with client.GetTask method.
-func (c *Client) DeleteUsers(ctx context.Context, userIDs []string, options DeleteUserOptions) (string, error) {
+// It returns an AsyncTaskResponse object which contains the task ID, the status of the task can be check with client.GetTask method.
+func (c *Client) DeleteUsers(ctx context.Context, userIDs []string, options DeleteUserOptions) (*AsyncTaskResponse, error) {
 	if len(userIDs) == 0 {
-		return "", fmt.Errorf("userIDs parameter should not be empty")
+		return nil, fmt.Errorf("userIDs parameter should not be empty")
 	}
 
 	data := struct {
@@ -102,7 +104,7 @@ func (c *Client) DeleteUsers(ctx context.Context, userIDs []string, options Dele
 
 	var resp AsyncTaskResponse
 	err := c.makeRequest(ctx, http.MethodPost, "users/delete", nil, data, &resp)
-	return resp.TaskID, err
+	return &resp, err
 }
 
 type ExportableChannel struct {
@@ -112,16 +114,16 @@ type ExportableChannel struct {
 	MessagesUntil *time.Time `json:"messages_until,omitempty"`
 }
 
-// ExportChannels requests an asynchronous export of the provided channels and returns
-// the ID of task.
-func (c *Client) ExportChannels(ctx context.Context, channels []*ExportableChannel, clearDeletedMessageText, includeTruncatedMessages *bool) (string, error) {
+// ExportChannels requests an asynchronous export of the provided channels.
+// It returns an AsyncTaskResponse object which contains the task ID, the status of the task can be check with client.GetTask method.
+func (c *Client) ExportChannels(ctx context.Context, channels []*ExportableChannel, clearDeletedMessageText, includeTruncatedMessages *bool) (*AsyncTaskResponse, error) {
 	if len(channels) == 0 {
-		return "", errors.New("number of channels must be at least one")
+		return nil, errors.New("number of channels must be at least one")
 	}
 
 	err := verifyExportableChannels(channels)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	req := struct {
@@ -136,7 +138,7 @@ func (c *Client) ExportChannels(ctx context.Context, channels []*ExportableChann
 
 	var resp AsyncTaskResponse
 	err = c.makeRequest(ctx, http.MethodPost, "export_channels", nil, req, &resp)
-	return resp.TaskID, err
+	return &resp, err
 }
 
 func verifyExportableChannels(channels []*ExportableChannel) error {
@@ -149,15 +151,14 @@ func verifyExportableChannels(channels []*ExportableChannel) error {
 }
 
 // GetExportChannelsTask returns current state of the export task.
-func (c *Client) GetExportChannelsTask(ctx context.Context, taskID string) (*Task, error) {
-	task := &Task{}
-
+func (c *Client) GetExportChannelsTask(ctx context.Context, taskID string) (*TaskResponse, error) {
 	if taskID == "" {
-		return task, errors.New("task ID must be not empty")
+		return nil, errors.New("task ID must be not empty")
 	}
 
 	p := path.Join("export_channels", url.PathEscape(taskID))
 
-	err := c.makeRequest(ctx, http.MethodGet, p, nil, nil, task)
-	return task, err
+	var task TaskResponse
+	err := c.makeRequest(ctx, http.MethodGet, p, nil, nil, &task)
+	return &task, err
 }
