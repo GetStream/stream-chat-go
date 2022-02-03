bfd45fc28d1574fae0e2c648efb470c3f8508192
peterdeme <snypox@gmail.com>
2022-02-03 11:58:51 +0100

    ci: bump go.mod file

--- .versionrc.js
+++ .versionrc.js
@@ -16,12 +16,30 @@ const versionFileUpdater = {
         const [major, minor, patch] = [splitted[0], splitted[1], splitted[2]];
 
         return contents
-            .replace(this.MAJOR_REGEX.exec(contents)[0], `versionMajor = ${major}`)
-            .replace(this.MINOR_REGEX.exec(contents)[0], `versionMinor = ${minor}`)
-            .replace(this.PATCH_REGEX.exec(contents)[0], `versionPatch = ${patch}`);
+            .replace(this.MAJOR_REGEX, `versionMajor = ${major}`)
+            .replace(this.MINOR_REGEX, `versionMinor = ${minor}`)
+            .replace(this.PATCH_REGEX, `versionPatch = ${patch}`);
+    }
+}
+
+const moduleVersionUpdater = {
+    GO_MOD_REGEX: /stream-chat-go\/v(\d+)/g,
+
+    readVersion: function (contents) {
+        return this.GO_MOD_REGEX.exec(contents)[1];
+    },
+
+    writeVersion: function (contents, version) {
+        const major = version.split('.')[0];
+
+        return contents.replace(this.GO_MOD_REGEX, `stream-chat-go/v${major}`);
     }
 }
 
 module.exports = {
-    bumpFiles: [{ filename: './version.go', updater: versionFileUpdater }],
+    bumpFiles: [
+        { filename: './version.go', updater: versionFileUpdater },
+        { filename: './go.mod', updater: moduleVersionUpdater },
+        { filename: './README.md', updater: moduleVersionUpdater },
+    ],
 }
